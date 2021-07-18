004b5aef4783331716d85e94050cb07ee901f0b3
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-18 17:59:21 +0300

    Created models: FoodModel, OrderModel, OrderCreateModel (to talk to api), OrderUpdateModel in order to Blazor has no connection to SQL

---
+++ BlazorClientDinerApp/Models/OrderCreateResultsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClientDinerApp.Models
+{
+    public class OrderCreateResultsModel
+    {
+        public int Id { get; set; }
+    }
+}
