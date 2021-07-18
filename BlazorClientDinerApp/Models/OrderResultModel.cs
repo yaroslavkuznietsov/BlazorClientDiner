8f96e5703280e335df458af5cfc7fd37aa83b61e
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-18 18:30:35 +0300

    Created OrderResultModel for Display Order Page

---
+++ BlazorClientDinerApp/Models/OrderResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClientDinerApp.Models
+{
+    public class OrderResultModel
+    {
+        public OrderModel Order { get; set; }
+        public string ItemPurchased { get; set; }
+    }
+}
