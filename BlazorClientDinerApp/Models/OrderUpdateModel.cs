16c979f6e22a0031d6a60905c857add2aa45434d
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2021-07-18 19:43:09 +0300

    Added Validation for OrderName into OrderUpdateModel

--- BlazorClientDinerApp/Models/OrderUpdateModel.cs
+++ BlazorClientDinerApp/Models/OrderUpdateModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,11 @@ namespace BlazorClientDinerApp.Models
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to a max of 20 characters")]
+        [MinLength(3, ErrorMessage = "You need to keep the name to a min of 3 characters")]
+        [DisplayName("Name for the Order")]
         public string OrderName { get; set; }
     }
 }
